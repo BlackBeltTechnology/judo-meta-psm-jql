6304b6b8ce000d258e0287d6460921691f85e22f
Robert Csakany <robert.csakany@blackbelt.hu>
2019-04-03 17:54:15 +0200

    Add save for model

--- runtime/src/main/java/hu/blackbelt/judo/meta/psm/jql/runtime/PsmJqlModelLoader.java
+++ runtime/src/main/java/hu/blackbelt/judo/meta/psm/jql/runtime/PsmJqlModelLoader.java
@@ -8,6 +8,7 @@ import org.eclipse.emf.ecore.resource.URIHandler;
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryRegistryImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.URIHandlerImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import java.io.IOException;
@@ -75,6 +76,27 @@ public class PsmJqlModelLoader {
             b.metaVersionRange(acceptedMetaVersionRange);
         }
         return b.build();
+    }
+
+
+    public static Map<Object, Object> getPsmJqlModelDefaultSaveOptions() {
+        Map<Object, Object> saveOptions = new HashMap<>();
+        saveOptions.put("DECLARE_XML", Boolean.TRUE);
+        saveOptions.put("PROCESS_DANGLING_HREF", "DISCARD");
+        saveOptions.put("URI_HANDLER", new URIHandlerImpl() {
+            public URI deresolve(URI uri) {
+                return uri.hasFragment() && uri.hasOpaquePart() && this.baseURI.hasOpaquePart() && uri.opaquePart().equals(this.baseURI.opaquePart()) ? URI.createURI("#" + uri.fragment()) : super.deresolve(uri);
+            }
+        });
+        saveOptions.put("SCHEMA_LOCATION", Boolean.TRUE);
+        saveOptions.put("DEFER_IDREF_RESOLUTION", Boolean.TRUE);
+        saveOptions.put("SKIP_ESCAPE_URI", Boolean.FALSE);
+        saveOptions.put("ENCODING", "UTF-8");
+        return saveOptions;
+    }
+
+    public static void savePsmJqlsModel(PsmJqlModel psmJqlModel) throws IOException {
+        psmJqlModel.getResourceSet().getResource(psmJqlModel.getUri(), false).save(getPsmJqlModelDefaultSaveOptions());
     }
 
 }
