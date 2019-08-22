d47dfb3ee95d127e0316a07a2b2134e801e55d16
Robert Csakany <robert.csakany@blackbelt.hu>
2019-08-22 11:07:30 +0200

    JNG-200 Add osgi integration smoke test - model tracker

---
+++ osgi-itest/src/test/java/hu/blackbelt/judo/meta/jql/osgi/itest/JqlModelLoadITest.java
@@ -0,0 +1,58 @@
+package hu.blackbelt.judo.meta.jql.osgi.itest;
+
+import hu.blackbelt.osgi.utils.osgi.api.BundleTrackerManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+
+import javax.inject.Inject;
+import java.io.*;
+
+import static hu.blackbelt.judo.meta.jql.osgi.itest.JqlKarafFeatureProvider.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.swissbox.core.BundleUtils.getBundle;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class JqlModelLoadITest {
+
+
+    public static final String HU_BLACKBELT_JUDO_META_JQL_OSGI = "hu.blackbelt.judo.meta.jql.osgi";
+
+    @Inject
+    LogService log;
+
+    @Inject
+    protected BundleTrackerManager bundleTrackerManager;
+
+    @Inject
+    BundleContext bundleContext;
+
+    @Configuration
+    public Option[] config() throws FileNotFoundException {
+
+        return combine(getRuntimeFeaturesForMetamodel(this.getClass()),
+                mavenBundle(maven()
+                        .groupId("hu.blackbelt.judo.meta")
+                        .artifactId("hu.blackbelt.judo.meta.jql.osgi")
+                        .versionAsInProject())) ;
+    }
+
+
+    @Test
+    public void testBundleActive() {
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_JQL_OSGI));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_JQL_OSGI)
+                .getState());
+    }
+}
